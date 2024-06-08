cbe787c508e689b4598f4d3e363afcbf87cf387e
Indraneel Chakraborty <64887729+ineelhere@users.noreply.github.com>
2024-06-08 07:56:52 +0530

    [ADD] option menu in sidebar

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from ui_components import display_chat, display_generate, display_list, display_show, display_create, display_copy, display_delete, display_pull, display_push, display_embeddings
 
-# Title
-st.title('LitOllama')
-
-# Create a horizontal option menu
-feature = option_menu(
-    menu_title=None,  # Hide the menu title
-    options=["Chat", "Generate", "List", "Show", "Create", "Copy", "Delete", "Pull", "Push", "Embeddings"],
-    icons=['chat', 'file-earmark-plus', 'list', 'eye', 'file-earmark', 'files', 'trash', 'cloud-arrow-down', 'cloud-arrow-up', 'graph-up'],
-    menu_icon="cast",
-    default_index=0,
-)
+with st.sidebar:
+    st.title('LitOllama')
+    feature = option_menu(
+        menu_title=None,  # Hide the menu title
+        options=["Chat", "Generate", "List", "Show", "Create", "Copy", "Delete", "Pull", "Push", "Embeddings"],
+        icons=['chat', 'file-earmark-plus', 'list', 'eye', 'file-earmark', 'files', 'trash', 'cloud-arrow-down', 'cloud-arrow-up', 'graph-up'],
+        menu_icon="cast",
+        default_index=0,
+    )
 
 # Display selected feature
 if feature == "Chat":
